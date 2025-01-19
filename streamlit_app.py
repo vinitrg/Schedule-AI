f54eba881f959a8b959a2052a3c3dc450efd528e
Vinit <vinit.gujarathi@gmail.com>
2025-01-19 05:11:46 +0000

    First commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 Interact with your Construction Schedule")
+st.write( "I'm being made smarter everyday by Vinit.")
